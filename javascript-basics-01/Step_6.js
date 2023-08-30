ccee6d5d4ba3f854beb53c45294db1059d6bdd3f
alialahmad68 <ali.alahmad.cs@gmail.com>
2023-08-30 16:35:33 +0300

    Step_6

---
+++ javascript-basics-01/Step_6.js
@@ -0,0 +1,10 @@
+
+
+function Calculate(){
+let first_number = parseFloat(document.getElementById("first_number").value);
+let second_number = parseFloat(document.getElementById("second_number").value);
+
+let result=first_number%second_number;
+
+alert("Result of Remainder is: "+result);
+}
