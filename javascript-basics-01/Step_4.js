a177a3cd2b17c8623de332e4615af30acb8d8a6b
alialahmad68 <ali.alahmad.cs@gmail.com>
2023-08-30 14:51:53 +0300

    Step_4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,5 @@
+let name = prompt("Please enter your Name");
+let surname = prompt("Please enter your Surname");
+let city = prompt("Please enter your City");
+
+alert("Name : "+name+"\nSurname : "+surname+"\nCity : "+city);
