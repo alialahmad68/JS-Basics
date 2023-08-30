91a9b1ff92368fc0047f21a96dcb23fd4d474953
alialahmad68 <ali.alahmad.cs@gmail.com>
2023-08-30 15:52:45 +0300

    Step_4 edited

--- javascript-basics-01/Step_4.js
+++ javascript-basics-01/Step_4.js
@@ -1,5 +1,8 @@
-let name = prompt("Please enter your Name");
-let surname = prompt("Please enter your Surname");
-let city = prompt("Please enter your City");
 
+
+function show(){
+let name = document.getElementById("name").value;
+let surname = document.getElementById("surname").value;
+let city = document.getElementById("city").value;
 alert("Name : "+name+"\nSurname : "+surname+"\nCity : "+city);
+}
