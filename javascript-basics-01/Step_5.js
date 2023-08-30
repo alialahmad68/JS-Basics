6fa2ed4e47a200b673fc071adc3cc92d720714c1
alialahmad68 <ali.alahmad.cs@gmail.com>
2023-08-30 16:04:04 +0300

    Step_5

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,8 @@
+
+
+function Calculate(){
+let first_number = parseInt(document.getElementById("first_number").value);
+let second_number = parseInt(document.getElementById("second_number").value);
+let result=first_number*second_number;
+alert("Result of multiplication is: "+result);
+}
